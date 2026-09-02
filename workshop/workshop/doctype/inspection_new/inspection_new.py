ad463812478912de62cbb58fdd4a82137d1b8782
ravi854 <ravi@a1its.net>
2026-09-02 10:27:33 +0000

    update changes and customization

--- workshop/workshop/doctype/inspection_new/inspection_new.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, A1ITS and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class InspectionNew(Document):
-	pass
